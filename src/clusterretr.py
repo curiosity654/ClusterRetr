64f8b554c1b09221134f042c6418af140bf90e7d
curiosity654 <Curiosity_wu@outlook.com>
2022-07-29 16:27:23 +0000

    1. add channel clustering 2. add symmetric distance calculation 3. add distance fuse

--- src/clusterretr.py
+++ src/clusterretr.py
@@ -3,12 +3,14 @@ import pickle
 import os
 import numpy as np
 from utils import nanopq
+from fcmeans import FCM
 from utils.tools import eval_precision, eval_AP_inner, compressITQ
 from scipy.spatial.distance import cdist
+import torch
 from tqdm import tqdm
 from time import time
 
-def expriment(X, gt_labels_gallery, predicted_features_query, gt_labels_query, _, M=1, Ks=256):
+def expriment(X, gt_labels_gallery, predicted_features_query, gt_labels_query, _, M=1, Ks=256, **args):
     pq = nanopq.PQ(M=M, verbose=False, Ks=Ks)
 
     pq.fit(X)
@@ -43,7 +45,143 @@ def expriment(X, gt_labels_gallery, predicted_features_query, gt_labels_query, _
     print("{}, {}, {}, {},".format(M, Ks, mAP_binary, prec_binary))
     return mAP_binary, prec_binary
 
-def itq_expriment(X, gt_labels_gallery, predicted_features_query, gt_labels_query, _, M=1, Ks=256,):
+def sym_expriment(X, gt_labels_gallery, predicted_features_query, gt_labels_query, _, M=1, Ks=256, **args):
+    pq = nanopq.PQ(M=M, verbose=False, Ks=Ks)
+
+    pq.fit(X)
+
+    X_code = pq.encode(X)
+    y_code = pq.encode(predicted_features_query)
+    binary_scores = cdist(y_code, X_code)
+
+    mAP_ls_binary = [[] for _ in range(len(np.unique(gt_labels_query)))]
+    for fi in range(predicted_features_query.shape[0]):
+        mapi_binary = eval_AP_inner(gt_labels_query[fi], binary_scores[fi], gt_labels_gallery)
+        mAP_ls_binary[gt_labels_query[fi]].append(mapi_binary)
+    
+    # for mAPi,mAPs in enumerate(mAP_ls):
+    #     print(str(mAPi)+' '+str(np.nanmean(mAPs))+' '+str(np.nanstd(mAPs)))
+    mAP_binary = np.array([np.nanmean(maps) for maps in mAP_ls_binary]).mean()
+    # print('mAP - hash: {:.4f}'.format(mAP_binary))
+    # wandb.log({"test/sketchy/mAP/all_binary": mAP_binary})
+
+    prec_ls_binary = [[] for _ in range(len(np.unique(gt_labels_query)))]
+    for fi in range(predicted_features_query.shape[0]):
+        prec_binary = eval_precision(gt_labels_query[fi], binary_scores[fi], gt_labels_gallery)
+        prec_ls_binary[gt_labels_query[fi]].append(prec_binary)
+
+    prec_binary = np.array([np.nanmean(pre) for pre in prec_ls_binary]).mean()
+    # print('Precision - hash: {:.4f}'.format(prec_binary))
+    # wandb.log({"test/sketchy/precision/all_binary": prec_binary})
+    print("{}, {}, {}, {},".format(M, Ks, mAP_binary, prec_binary))
+    return mAP_binary, prec_binary
+
+def cpq_expriment(X, gt_labels_gallery, predicted_features_query, gt_labels_query, _, M=1, Ks=256, **args):
+    pq = nanopq.CPQ(M=M, verbose=False, Ks=Ks)
+
+    pq.fit(X)
+
+    X_code = pq.encode(X)
+    binary_scores = []
+    for query in predicted_features_query:
+        dists = pq.dtable(query).adist(X_code)
+        binary_scores.append(dists)
+
+    binary_scores = np.stack(binary_scores)
+
+    mAP_ls_binary = [[] for _ in range(len(np.unique(gt_labels_query)))]
+    for fi in range(predicted_features_query.shape[0]):
+        mapi_binary = eval_AP_inner(gt_labels_query[fi], binary_scores[fi], gt_labels_gallery)
+        mAP_ls_binary[gt_labels_query[fi]].append(mapi_binary)
+    
+    # for mAPi,mAPs in enumerate(mAP_ls):
+    #     print(str(mAPi)+' '+str(np.nanmean(mAPs))+' '+str(np.nanstd(mAPs)))
+    mAP_binary = np.array([np.nanmean(maps) for maps in mAP_ls_binary]).mean()
+    # print('mAP - hash: {:.4f}'.format(mAP_binary))
+    # wandb.log({"test/sketchy/mAP/all_binary": mAP_binary})
+
+    prec_ls_binary = [[] for _ in range(len(np.unique(gt_labels_query)))]
+    for fi in range(predicted_features_query.shape[0]):
+        prec_binary = eval_precision(gt_labels_query[fi], binary_scores[fi], gt_labels_gallery)
+        prec_ls_binary[gt_labels_query[fi]].append(prec_binary)
+
+    prec_binary = np.array([np.nanmean(pre) for pre in prec_ls_binary]).mean()
+    # print('Precision - hash: {:.4f}'.format(prec_binary))
+    # wandb.log({"test/sketchy/precision/all_binary": prec_binary})
+    print("{}, {}, {}, {},".format(M, Ks, mAP_binary, prec_binary))
+    return mAP_binary, prec_binary
+
+def fuse_expriment(X, gt_labels_gallery, predicted_features_query, gt_labels_query, _, M=1, Ks=256, lam=0.2):
+    pq = nanopq.PQ(M=M, verbose=False, Ks=Ks)
+
+    pq.fit(X)
+    scores = cdist(predicted_features_query, X)
+    X_code = pq.encode(X)
+    binary_scores = []
+    for query in predicted_features_query:
+        dists = pq.dtable(query).adist(X_code)
+        binary_scores.append(dists)
+
+    binary_scores = np.stack(binary_scores)
+    # lam = args["lam"]
+    binary_scores = (1-lam) * binary_scores + lam * scores
+
+    mAP_ls_binary = [[] for _ in range(len(np.unique(gt_labels_query)))]
+    for fi in range(predicted_features_query.shape[0]):
+        mapi_binary = eval_AP_inner(gt_labels_query[fi], binary_scores[fi], gt_labels_gallery)
+        mAP_ls_binary[gt_labels_query[fi]].append(mapi_binary)
+    
+    # for mAPi,mAPs in enumerate(mAP_ls):
+    #     print(str(mAPi)+' '+str(np.nanmean(mAPs))+' '+str(np.nanstd(mAPs)))
+    mAP_binary = np.array([np.nanmean(maps) for maps in mAP_ls_binary]).mean()
+    # print('mAP - hash: {:.4f}'.format(mAP_binary))
+    # wandb.log({"test/sketchy/mAP/all_binary": mAP_binary})
+
+    prec_ls_binary = [[] for _ in range(len(np.unique(gt_labels_query)))]
+    for fi in range(predicted_features_query.shape[0]):
+        prec_binary = eval_precision(gt_labels_query[fi], binary_scores[fi], gt_labels_gallery)
+        prec_ls_binary[gt_labels_query[fi]].append(prec_binary)
+
+    prec_binary = np.array([np.nanmean(pre) for pre in prec_ls_binary]).mean()
+    # print('Precision - hash: {:.4f}'.format(prec_binary))
+    # wandb.log({"test/sketchy/precision/all_binary": prec_binary})
+    print("{}, {}, {}, {},".format(M, Ks, mAP_binary, prec_binary))
+    return mAP_binary, prec_binary
+
+def fkmeans_expriment(X, gt_labels_gallery, predicted_features_query, gt_labels_query, _scores, _M=1, Ks=256, **args):
+    fcm = FCM(n_clusters=Ks)
+    fcm.fit(X)
+
+    centers = fcm.centers
+    # soft_labels = fcm.u
+    soft_labels = np.eye(Ks)[fcm.u.argmax(axis=-1)]
+    binary_features_gallery = np.matmul(soft_labels, centers)
+    binary_scores = cdist(predicted_features_query, binary_features_gallery)
+    # binary_scores = torch.cdist(torch.from_numpy(predicted_features_query).float().cuda(), torch.from_numpy(binary_features_gallery).float().cuda()).cpu().numpy()
+
+    mAP_ls_binary = [[] for _ in range(len(np.unique(gt_labels_query)))]
+    for fi in range(predicted_features_query.shape[0]):
+        mapi_binary = eval_AP_inner(gt_labels_query[fi], binary_scores[fi], gt_labels_gallery)
+        mAP_ls_binary[gt_labels_query[fi]].append(mapi_binary)
+    
+    # for mAPi,mAPs in enumerate(mAP_ls):
+    #     print(str(mAPi)+' '+str(np.nanmean(mAPs))+' '+str(np.nanstd(mAPs)))
+    mAP_binary = np.array([np.nanmean(maps) for maps in mAP_ls_binary]).mean()
+    # print('mAP - hash: {:.4f}'.format(mAP_binary))
+    # wandb.log({"test/sketchy/mAP/all_binary": mAP_binary})
+
+    prec_ls_binary = [[] for _ in range(len(np.unique(gt_labels_query)))]
+    for fi in range(predicted_features_query.shape[0]):
+        prec_binary = eval_precision(gt_labels_query[fi], binary_scores[fi], gt_labels_gallery)
+        prec_ls_binary[gt_labels_query[fi]].append(prec_binary)
+
+    prec_binary = np.array([np.nanmean(pre) for pre in prec_ls_binary]).mean()
+    # print('Precision - hash: {:.4f}'.format(prec_binary))
+    # wandb.log({"test/sketchy/precision/all_binary": prec_binary})
+    print("{}, {}, {}".format(Ks, mAP_binary, prec_binary))
+    return mAP_binary, prec_binary
+
+def itq_expriment(X, gt_labels_gallery, predicted_features_query, gt_labels_query, _, M=1, Ks=256, **args):
     pq = nanopq.PQ(M=M, verbose=False, Ks=Ks)
 
     pq_X = pq.fit(X)
@@ -68,7 +206,7 @@ def itq_expriment(X, gt_labels_gallery, predicted_features_query, gt_labels_quer
     print("{}, {}, {}, {},".format(M, Ks, mAP_binary, prec_binary))
     return mAP_binary, prec_binary
 
-def reranking_expriment(X, gt_labels_gallery, predicted_features_query, gt_labels_query, scores, M=1, Ks=256,):
+def reranking_expriment(X, gt_labels_gallery, predicted_features_query, gt_labels_query, scores, M=1, Ks=256, **args):
     pq = nanopq.PQ(M=M, verbose=False, Ks=Ks)
 
     pq.fit(X)
@@ -118,12 +256,12 @@ def reranking_expriment(X, gt_labels_gallery, predicted_features_query, gt_label
     print("{}, {}, {}, {},".format(M, Ks, mAP_binary, prec_binary))
     return mAP_binary, prec_binary
 
-def run_experiments(expriment, predicted_features_gallery, gt_labels_gallery, predicted_features_query, gt_labels_query, scores, Ms, Ks,):
+def run_experiments(expriment, predicted_features_gallery, gt_labels_gallery, predicted_features_query, gt_labels_query, scores, Ms, Ks, lam):
     from multiprocessing import Process
     processes = []
     for M in Ms:
         for K in Ks:
-            exp = Process(target=expriment, args=(predicted_features_gallery, gt_labels_gallery, predicted_features_query, gt_labels_query, scores, M, K,))
+            exp = Process(target=expriment, args=(predicted_features_gallery, gt_labels_gallery, predicted_features_query, gt_labels_query, scores, M, K, lam))
             processes.append(exp)
 
     [p.start() for p in processes]
@@ -158,7 +296,12 @@ if __name__ =='__main__':
     parser.add_argument('--itq', action='store_true', default=False)
     parser.add_argument('--rerank', action='store_true', default=False)
     parser.add_argument('--real', action='store_true', default=False)
+    parser.add_argument('--fcm', action='store_true', default=False)
+    parser.add_argument('--fuse', type=float, default=-1)
+    parser.add_argument('--cpq', action='store_true', default=False)
+    parser.add_argument('--sym', action='store_true', default=False)
     parser.add_argument('--time', action='store_true', default=False)
+    parser.add_argument('--debug', action='store_true', default=False)
 
     args = parser.parse_args()
     with open(args.feature_file, 'rb') as fh:
@@ -170,24 +313,38 @@ if __name__ =='__main__':
         # print(predicted_features_gallery.shape)
         # scores = cdist(predicted_features_query, predicted_features_gallery)
 
-    # Ms = [1, 2, 4, 8, 16]
-    # Ks = [8, 16, 25, 32, 64, 128, 256]
+    Ks = [8, 16, 25, 32, 64, 128, 256]
     Ms = [1, 2, 4, 8, 16]
-    Ks = [32]
+    # Ms = [1]
+    # Ks = [32]
+    # Ks = [30]
     # Ks = [8, 16, 32]
     if args.time:
-        print("M, K, Time,")
-        for M in Ms:
-            for K in Ks:
-                time_pq_computation(predicted_features_gallery, predicted_features_query, M, K)    
+        if args.real:
+            time_real_computation(predicted_features_gallery, predicted_features_query)
+        else:
+            print("M, K, Time,")
+            for M in Ms:
+                for K in Ks:
+                    time_pq_computation(predicted_features_gallery, predicted_features_query, M, K)
     else:
         print("M, K, mAP, Prec,")
     if args.itq:
         run_experiments(itq_expriment, predicted_features_gallery, gt_labels_gallery, predicted_features_query, gt_labels_query, scores, Ms, Ks)
     elif args.rerank:
         run_experiments(reranking_expriment, predicted_features_gallery, gt_labels_gallery, predicted_features_query, gt_labels_query, scores, Ms, Ks)
-    elif args.real:
-        time_real_computation(predicted_features_gallery, predicted_features_query)
+    elif args.fcm:
+        run_experiments(fkmeans_expriment, predicted_features_gallery, gt_labels_gallery, predicted_features_query, gt_labels_query, scores, Ms, Ks)
+    elif args.fuse > 0:
+        run_experiments(fuse_expriment, predicted_features_gallery, gt_labels_gallery, predicted_features_query, gt_labels_query, scores, Ms, Ks, lam=0.5)
+    elif args.cpq:
+        run_experiments(cpq_expriment, predicted_features_gallery, gt_labels_gallery, predicted_features_query, gt_labels_query, scores, Ms, Ks)
+    elif args.sym:
+        run_experiments(sym_expriment, predicted_features_gallery, gt_labels_gallery, predicted_features_query, gt_labels_query, scores, Ms, Ks)
+    elif args.debug:
+        cpq_expriment(predicted_features_gallery, gt_labels_gallery, predicted_features_query, gt_labels_query, scores, 4, 32)
+        # fuse_expriment(predicted_features_gallery, gt_labels_gallery, predicted_features_query, gt_labels_query, scores, 1, 32, 0.2)
+        # fkmeans_expriment(predicted_features_gallery, gt_labels_gallery, predicted_features_query, gt_labels_query, scores, 1, 32)
+        # expriment(predicted_features_gallery, gt_labels_gallery, predicted_features_query, gt_labels_query, 1, 256)
     else:
         run_experiments(expriment, predicted_features_gallery, gt_labels_gallery, predicted_features_query, gt_labels_query, scores, Ms, Ks)
-    # expriment(predicted_features_gallery, gt_labels_gallery, predicted_features_query, gt_labels_query, 1, 256)
--- src/utils/nanopq/pq.py
+++ src/utils/nanopq/pq.py
@@ -1,5 +1,12 @@
 import numpy as np
 from scipy.cluster.vq import kmeans2, vq
+from scipy import stats
+import hdbscan
+from sklearn.cluster import SpectralClustering, AgglomerativeClustering
+from sklearn.mixture import GaussianMixture
+from sqlalchemy import true
+from torch_scatter import scatter_mean
+import torch
 
 class PQ(object):
     """Pure python implementation of Product Quantization (PQ) [Jegou11]_.
@@ -39,7 +46,7 @@ class PQ(object):
             np.uint8 if Ks <= 2 ** 8 else (np.uint16 if Ks <= 2 ** 16 else np.uint32)
         )
         self.codewords = None
-        self.lables = None
+        self.labels = None
         self.Ds = None
 
         if verbose:
@@ -86,18 +93,47 @@ class PQ(object):
 
         # [m][ks][ds]: m-th subspace, ks-the codeword, ds-th dim
         self.codewords = np.zeros((self.M, self.Ks, self.Ds), dtype=np.float32)
-        self.lables = np.zeros((self.M, N), dtype=np.float32)
+        self.labels = np.zeros((self.M, N), dtype=np.float32)
         self.revec = np.zeros_like(vecs)
         for m in range(self.M):
             if self.verbose:
                 print("Training the subspace: {} / {}".format(m, self.M))
             vecs_sub = vecs[:, m * self.Ds : (m + 1) * self.Ds]
             # self.codewords[m], a = kmeans2(vecs_sub, self.Ks, iter=iter, minit="points")
-            self.codewords[m], self.lables[m] = kmeans2(vecs_sub, self.Ks, iter=iter, minit="points")
+            # KMeans
+            self.codewords[m], self.labels[m] = kmeans2(vecs_sub, self.Ks, iter=iter, minit="points")
+            
+            # HDBSCAN
+            # clusterer = hdbscan.HDBSCAN(min_cluster_size=10)
+            # cluster_labels = clusterer.fit_predict(vecs_sub)
+            # self.labels[m] = cluster_labels
+            # src = torch.Tensor(vecs_sub[cluster_labels>0])
+            # index = torch.from_numpy(cluster_labels[cluster_labels>0])
+            # out = scatter_mean(src, index, dim=0)
+            # self.codewords[m] = out
+
+            # Spectral Clustering
+            # clusterer = SpectralClustering(n_clusters=self.Ks, assign_labels='discretize', random_state=0)
+            # cluster_labels = clusterer.fit_predict(vecs_sub)
+            # self.labels[m] = cluster_labels
+            # src = torch.Tensor(vecs_sub[cluster_labels>0])
+            # index = torch.from_numpy(cluster_labels[cluster_labels>0])
+            # out = scatter_mean(src, index, dim=0)
+            # self.codewords[m] = out
+
+            # GMM
+            # clusterer = GaussianMixture(n_components=self.Ks, random_state=0)
+            # cluster_labels = clusterer.fit_predict(vecs_sub)
+            # self.labels[m] = cluster_labels
+            # src = torch.Tensor(vecs_sub[cluster_labels>0])
+            # index = torch.from_numpy(cluster_labels[cluster_labels>0])
+            # out = scatter_mean(src, index, dim=0)
+            # self.codewords[m] = out
+            
         
         for m in range(self.M):
             for n in range(N):
-                self.revec[n][m * self.Ds: (m + 1) * self.Ds] = self.codewords[m][int(self.lables[m][n])]
+                self.revec[n][m * self.Ds: (m + 1) * self.Ds] = self.codewords[m][int(self.labels[m][n])]
 
         return self.revec
 
@@ -176,6 +212,245 @@ class PQ(object):
         dtable = np.empty((self.M, self.Ks), dtype=np.float32)
         for m in range(self.M):
             query_sub = query[m * self.Ds : (m + 1) * self.Ds]
+            dtable[m, :] = np.linalg.norm(self.codewords[m] - query_sub, axis=1) ** 2
+
+        return DistanceTable(dtable)
+
+
+class DistanceTable(object):
+    """Distance table from query to codeworkds.
+    Given a query vector, a PQ/OPQ instance compute this DistanceTable class
+    using :func:`PQ.dtable` or :func:`OPQ.dtable`.
+    The Asymmetric Distance from query to each database codes can be computed
+    by :func:`DistanceTable.adist`.
+
+    Args:
+        dtable (np.ndarray): Distance table with shape=(M, Ks) and dtype=np.float32
+            computed by :func:`PQ.dtable` or :func:`OPQ.dtable`
+
+    Attributes:
+        dtable (np.ndarray): Distance table with shape=(M, Ks) and dtype=np.float32.
+            Note that dtable[m][ks] contains the squared Euclidean distance between
+            (1) m-th sub-vector of query and (2) ks-th codeword for m-th subspace.
+
+    """
+
+    def __init__(self, dtable):
+        assert dtable.ndim == 2
+        assert dtable.dtype == np.float32
+        self.dtable = dtable
+
+    def adist(self, codes):
+        """Given PQ-codes, compute Asymmetric Distances between the query (self.dtable)
+        and the PQ-codes.
+
+        Args:
+            codes (np.ndarray): PQ codes with shape=(N, M) and
+                dtype=pq.code_dtype where pq is a pq instance that creates the codes
+
+        Returns:
+            np.ndarray: Asymmetric Distances with shape=(N, ) and dtype=np.float32
+
+        """
+
+        assert codes.ndim == 2
+        N, M = codes.shape
+        assert M == self.dtable.shape[0]
+
+        # Fetch distance values using codes. The following codes are
+        dists = np.sum(self.dtable[range(M), codes], axis=1)
+
+        # The above line is equivalent to the followings:
+        # dists = np.zeros((N, )).astype(np.float32)
+        # for n in range(N):
+        #     for m in range(M):
+        #         dists[n] += self.dtable[m][codes[n][m]]
+
+        return dists
+
+class CPQ(object):
+
+    def __init__(self, M, Ks=256, verbose=True):
+        assert 0 < Ks <= 2 ** 32
+        self.M, self.Ks, self.verbose = M, Ks, verbose
+        self.code_dtype = (
+            np.uint8 if Ks <= 2 ** 8 else (np.uint16 if Ks <= 2 ** 16 else np.uint32)
+        )
+        self.codewords = None
+        self.labels = None
+        self.Ds = None
+
+        if verbose:
+            print("M: {}, Ks: {}, code_dtype: {}".format(M, Ks, self.code_dtype))
+
+    def __eq__(self, other):
+        if isinstance(other, PQ):
+            return (self.M, self.Ks, self.verbose, self.code_dtype, self.Ds) == (
+                other.M,
+                other.Ks,
+                other.verbose,
+                other.code_dtype,
+                other.Ds,
+            ) and np.array_equal(self.codewords, other.codewords)
+        else:
+            return False
+
+    def fit(self, vecs, iter=20, seed=123):
+        """Given training vectors, run k-means for each sub-space and create
+        codewords for each sub-space.
+
+        This function should be run once first of all.
+
+        Args:
+            vecs (np.ndarray): Training vectors with shape=(N, D) and dtype=np.float32.
+            iter (int): The number of iteration for k-means
+            seed (int): The seed for random process
+
+        Returns:
+            object: self
+
+        """
+        assert vecs.dtype == np.float32
+        assert vecs.ndim == 2
+        N, self.D = vecs.shape
+        assert self.Ks < N, "the number of training vector should be more than Ks"
+        assert self.D > self.M
+        self.revec = None
+
+        np.random.seed(seed)
+        if self.verbose:
+            print("iter: {}, seed: {}".format(iter, seed))
+
+        # [m][ks][ds]: m-th subspace, ks-the codeword, ds-th dim
+        # ccenters, self.clabels = kmeans2(vecs.T, self.M, iter=iter, minit="points")
+        clustering = SpectralClustering(self.M, affinity='precomputed', n_init=100, assign_labels='discretize')
+        # clustering = AgglomerativeClustering(n_clusters=self.M, affinity='precomputed', linkage='single')
+        rho, _ = stats.spearmanr(vecs)
+        # rho = np.corrcoef(vecs.T)
+        rho = 1 - abs(rho)
+        self.clabels = clustering.fit_predict(rho)
+
+        _, self.Ds = np.unique(self.clabels, return_counts=True)
+        self.codewords = [np.zeros((self.Ks, d), dtype=np.float32) for d in self.Ds]
+        self.labels = np.zeros((self.M, N), dtype=np.float32)
+        self.revec = np.zeros_like(vecs)
+        for m in range(self.M):
+            if self.verbose:
+                print("Training the subspace: {} / {}".format(m, self.M))
+            vecs_sub = vecs[:, self.clabels == m]
+            # self.codewords[m], a = kmeans2(vecs_sub, self.Ks, iter=iter, minit="points")
+            # KMeans
+            self.codewords[m], self.labels[m] = kmeans2(vecs_sub, self.Ks, iter=iter, minit="points")
+            
+            # HDBSCAN
+            # clusterer = hdbscan.HDBSCAN(min_cluster_size=10)
+            # cluster_labels = clusterer.fit_predict(vecs_sub)
+            # self.labels[m] = cluster_labels
+            # src = torch.Tensor(vecs_sub[cluster_labels>0])
+            # index = torch.from_numpy(cluster_labels[cluster_labels>0])
+            # out = scatter_mean(src, index, dim=0)
+            # self.codewords[m] = out
+
+            # Spectral Clustering
+            # clusterer = SpectralClustering(n_clusters=self.Ks, assign_labels='discretize', random_state=0)
+            # cluster_labels = clusterer.fit_predict(vecs_sub)
+            # self.labels[m] = cluster_labels
+            # src = torch.Tensor(vecs_sub[cluster_labels>0])
+            # index = torch.from_numpy(cluster_labels[cluster_labels>0])
+            # out = scatter_mean(src, index, dim=0)
+            # self.codewords[m] = out
+
+            # GMM
+            # clusterer = GaussianMixture(n_components=self.Ks, random_state=0)
+            # cluster_labels = clusterer.fit_predict(vecs_sub)
+            # self.labels[m] = cluster_labels
+            # src = torch.Tensor(vecs_sub[cluster_labels>0])
+            # index = torch.from_numpy(cluster_labels[cluster_labels>0])
+            # out = scatter_mean(src, index, dim=0)
+            # self.codewords[m] = out
+            
+        
+        for m in range(self.M):
+            for n in range(N):
+                self.revec[n][self.clabels == m] = self.codewords[m][int(self.labels[m][n])]
+
+        return self.revec
+
+    def encode(self, vecs):
+        """Encode input vectors into PQ-codes.
+
+        Args:
+            vecs (np.ndarray): Input vectors with shape=(N, D) and dtype=np.float32.
+
+        Returns:
+            np.ndarray: PQ codes with shape=(N, M) and dtype=self.code_dtype
+
+        """
+        assert vecs.dtype == np.float32
+        assert vecs.ndim == 2
+        N, D = vecs.shape
+        assert D == self.D, "input dimension must be D"
+
+        # codes[n][m] : code of n-th vec, m-th subspace
+        codes = np.empty((N, self.M), dtype=self.code_dtype)
+        for m in range(self.M):
+            if self.verbose:
+                print("Encoding the subspace: {} / {}".format(m, self.M))
+            vecs_sub = vecs[:, self.clabels == m]
+            codes[:, m], _ = vq(vecs_sub, self.codewords[m])
+
+        return codes
+
+    def decode(self, codes):
+        """Given PQ-codes, reconstruct original D-dimensional vectors
+        approximately by fetching the codewords.
+
+        Args:
+            codes (np.ndarray): PQ-cdoes with shape=(N, M) and dtype=self.code_dtype.
+                Each row is a PQ-code
+
+        Returns:
+            np.ndarray: Reconstructed vectors with shape=(N, D) and dtype=np.float32
+
+        """
+        assert codes.ndim == 2
+        N, M = codes.shape
+        assert M == self.M
+        assert codes.dtype == self.code_dtype
+
+        vecs = np.empty((N, self.D), dtype=np.float32)
+        for m in range(self.M):
+            vecs[:, self.clabels == m] = self.codewords[m][codes[:, m], :]
+
+        return vecs
+
+    def dtable(self, query):
+        """Compute a distance table for a query vector.
+        The distances are computed by comparing each sub-vector of the query
+        to the codewords for each sub-subspace.
+        `dtable[m][ks]` contains the squared Euclidean distance between
+        the `m`-th sub-vector of the query and the `ks`-th codeword
+        for the `m`-th sub-space (`self.codewords[m][ks]`).
+
+        Args:
+            query (np.ndarray): Input vector with shape=(D, ) and dtype=np.float32
+
+        Returns:
+            nanopq.DistanceTable:
+                Distance table. which contains
+                dtable with shape=(M, Ks) and dtype=np.float32
+
+        """
+        assert query.dtype == np.float32
+        assert query.ndim == 1, "input must be a single vector"
+        (D,) = query.shape
+        assert D == self.D, "input dimension must be D"
+
+        # dtable[m] : distance between m-th subvec and m-th codewords (m-th subspace)
+        # dtable[m][ks] : distance between m-th subvec and ks-th codeword of m-th codewords
+        dtable = np.empty((self.M, self.Ks), dtype=np.float32)
+        for m in range(self.M):
+            query_sub = query[self.clabels == m]
             dtable[m, :] = np.linalg.norm(self.codewords[m] - query_sub, axis=1) ** 2
 
         return DistanceTable(dtable)
